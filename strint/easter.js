ad6313bfbdf6eddcb712babd7b82cea690cda0fd
xTaurix <krzyzu2204@gmail.com>
2022-05-25 14:36:25 +0200

    eva

--- strint/easter.js
+++ strint/easter.js
@@ -2,17 +2,11 @@ var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'Arr
 var current = 0;
 
 var keyHandler = function (event) {
-
-	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
 	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
 		current = 0;
 		return;
 	}
-
-	// Update how much of the pattern is complete
 	current++;
-
-	// If complete, alert and reset
 	if (pattern.length === current) {
 		current = 0;
 		window.location.href = "easter.html";
