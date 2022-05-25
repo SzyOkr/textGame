0a3845e331bf68383af53f58f3bf5e030cab4bd5
xTaurix <krzyzu2204@gmail.com>
2022-05-25 14:07:46 +0200

    easter

--- strint/easter.js
+++ strint/easter.js
@@ -15,7 +15,7 @@ var keyHandler = function (event) {
 	// If complete, alert and reset
 	if (pattern.length === current) {
 		current = 0;
-		window.alert('cum monster');
+		window.location.href = "mapa.html";
 	}
 
 };
