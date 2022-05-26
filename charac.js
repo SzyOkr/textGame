2edc16189e5d97bc0055e0e339657c995320505a
SzyOkr <104886808+SzyOkr@users.noreply.github.com>
2022-05-26 09:33:49 +0200

    staty postaci

--- charac.js
+++ charac.js
@@ -1,10 +1,20 @@
 class Postac {
-constructor()
-nazwa.rycerza = klasa
-rycerz.HP =
-rycerz.ATK = ATK
-rycerz.SPD
-rycerz.STN
-rycerz.DEF
-rycerz.USG
+constructor(name, HP, ATK, SPD, STN, DEF, USG) {
+character.name = klasa
+character.HP = HP
+character.ATK = ATK
+character.SPD = SPD
+character.STN = STN
+character.DEF = DEF
+character.USG = USG
+}
+
+isAlive(){
+    return character.HP > 0
+}
+}
+
+class Rycerz extends Postac {
+    constructor(name, HP, ATK, SPD, STN, DEF, USG) {
+        super(name="Rycerz", HP=,ATK=,SPD=,STN=,DEF=,USG=,)
 }
